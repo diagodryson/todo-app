583ec2087298f2512361b9f8b181f115c4017415
diagodryson <cortezdiago@gmail.com>
2022-07-04 22:17:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,7 @@ document.querySelector('#push').onclick = function() {
     if(document.querySelector('#newtask input').value.length == 0){
         alert("Please Enter a Task");
     } 
-    // else{
-    //     document.querySelector('#tasks').innerHTML += '
-        
-        
-    //     ';
-    // }
+    else{
+         document.querySelector('#tasks').innerHTML += '<div class="task"><span id "taskname">${document.querySelector("#newtask input").value}</span><button class="delete">X<button>';
+    }
 }
