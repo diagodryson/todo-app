8b809e6694efe694e11694816f9ca48cc9a48b9c
diagodryson <cortezdiago@gmail.com>
2022-07-04 23:10:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.querySelector('#push').onclick = function() {
         ${document.querySelector("#newtask input").
         value}
         </span>
-        <button class="delete">X<button>
+        <button class="delete">X</button>
         </div>
         `;
 
